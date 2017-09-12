8d2c442757a68e86d1054d366475a60cd8c20d6f
Nick Franciosi <nickfranciosi@gmail.com>
2017-09-11 22:06:09 -0500

    temp spacing for home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,28 +5,51 @@ import Button from '../components/button';
 import logo from '../assets/images/LogoMainColor.png';
 
 export default () => (
-  <ViewPortView>
-    <Container>
-      <div 
+  <div>
+    <div 
         className="logo"
         style={{
           maxWidth: 120,
+          marginLeft: 87,
+          marginTop: 70,
         }}
       >
         <img src={logo} alt="Terminal Logo"/>
-      </div>
-      <div 
-        className="content"
-        style={{
-          width: "60%",
-        }}
-      >
-        <span>Tristique Parturient</span>
-        <h1>We build teams in emerging tech hubs around the world.</h1>
-        <p>Donec ullamcorper nulla non metus auctor fringilla. Etiam porta sem magna mollis euismod.</p>
-        <Button>Learn More</Button>
-      </div>
-    </Container>
-  </ViewPortView>
+    </div>
+    <ViewPortView>
+      <Container>
+        <div 
+          className="content"
+          style={{
+            width: "60%",
+          }}
+        >
+          <span
+            style={{
+              display: "inline-block",
+              marginBottom: 37,
+            }}
+          >
+            Tristique Parturient
+          </span>
+          <h1
+          style={{
+              marginBottom: 30,
+            }}
+          >
+            We build teams in emerging tech hubs around the world.
+          </h1>
+          <p
+          style={{
+              marginBottom: 73,
+            }}
+          >
+            Donec ullamcorper nulla non metus auctor fringilla. Etiam porta sem magna mollis euismod.
+          </p>
+          <Button>Learn More</Button>
+        </div>
+      </Container>
+    </ViewPortView>
+  </div>
 );
   
