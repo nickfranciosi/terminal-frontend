856435328f4bd1de2adb6f514844a7a570e64a7b
halvves <hi@halvves.com>
2017-09-26 04:57:33 -0400

    AngledImage always go to bottom center of viewport

--- src/components/angledImage/index.js
+++ src/components/angledImage/index.js
@@ -3,10 +3,8 @@ import PropTypes from 'prop-types';
 import styles from './style.module.css';
 
 const AngledImage = ({ src }) => (
-  <div className={styles.container}>
-    <div className={styles.imageWrap}>
-      <img className={styles.image} src={src} alt="" />
-    </div>
+  <div className={styles.imageOuter}>
+    <div className={styles.image} style={{backgroundImage: `url('${src}')`}} />
   </div>
 );
 
