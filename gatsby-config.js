6cf191ddfc124e1355e488b5c2615e32fccd8914
Nick Franciosi <nickfranciosi@gmail.com>
2017-10-13 14:55:13 -0500

    set up wp api

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,5 +10,29 @@ module.exports = {
         pathToConfigModule: `src/utils/typography.js`,
       },
     },
+    {
+      resolve: 'gatsby-source-wordpress',
+      options: {
+         /*
+        * The base URL of the Wordpress site without the trailingslash and the protocol. This is required.
+        * Example : 'gatsbyjsexamplewordpress.wordpress.com' or 'www.example-site.com'
+        */
+        baseUrl: '69a.ad7.mwp.accessdomain.com',
+        // The protocol. This can be http or https.
+        protocol: 'http',
+        // Indicates whether the site is hosted on wordpress.com.
+        // If false, then the asumption is made that the site is self hosted.
+        // If true, then the plugin will source its content on wordpress.com using the JSON REST API V2.
+        // If your site is hosted on wordpress.org, then set this to false.
+        hostingWPCOM: false,
+        // If useACF is true, then the source plugin will try to import the Wordpress ACF Plugin contents.
+        // This feature is untested for sites hosted on Wordpress.com.
+        // Defaults to true.
+        useACF: true,
+        // Set verboseOutput to true to display a verbose output on `npm run develop` or `npm run build`
+        // It can help you debug specific API Endpoints problems
+        verboseOutput: false,
+      },
+    },
   ],
 }
