f0d4396e9b83a72926c86dc4c902f032f595c408
Nick Franciosi <nickfranciosi@gmail.com>
2017-10-16 17:10:02 -0500

    fix mobile parallax issue

--- src/components/fullBackgroundImage/index.js
+++ src/components/fullBackgroundImage/index.js
@@ -4,12 +4,13 @@ import MediaQuery from 'react-responsive';
 import styles from './style.module.css';
 
 const FullBackgroundImage = ({ children, srcDesktop, srcMobile}) => (
-  <MediaQuery maxWidth={455}>
+  <MediaQuery maxWidth={650}>
     {(matches) => (
         <Parallax 
         bgImage={matches ? srcMobile : srcDesktop} 
         strength={200} 
         className={styles.container}
+        disabled={matches}
       > 
         {children}
       </Parallax>
